45ad3eb292cf07b943f32fab44262ce57096c6ef
Donia Tung <doniatung32@gmail.com>
2018-04-30 13:19:26 -0400

    lol

--- reduce.py
+++ reduce.py
@@ -4,5 +4,20 @@ story = "alice.txt"
 f = open(story, 'r')
 
 contents = f.read();
+contents = contents.split(" ")
+contents = [x.strip('\n') for x in contents]
+contents = [x.strip(',') for x in contents]
 
 print contents
+
+def counter(word):
+    reduce ((lambda x: x), word) 
+
+listOfWords = ["hello", "hello", "hi", "bye", "hello"]
+
+c = 0;
+def counting(word):
+    for x in listOfWords:
+        if(x == word):
+            c += 1;
+    return c
